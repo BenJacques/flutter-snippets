9b0c460b77618902d13d323860c4971537ff63c1
BenJacques <58220520+BenJacques@users.noreply.github.com>
2024-10-22 13:39:05 -0400

    Create character_list.dart

---
+++ character_list.dart
@@ -0,0 +1,14 @@
+  static const List<String> _characterList = [
+    'Gollum',
+    'Frodo Baggins',
+    'Samwise Gamgee',
+    'Aragorn II Elessar',
+    'Gandalf',
+    'Gimli',
+    'Legolas',
+    'Boromir',
+    'Meriadoc Brandybuck',
+    'Peregrin Took',
+    'Saruman',
+    'Treebeard',
+  ];
